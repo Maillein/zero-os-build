06e484fa7635c93d2c07de9bbea445a28e351e0d
Maillein <realshinya1999@gmail.com>
2023-01-05 20:22:43 +0900

    day01まで実装

--- day01/c/hello.c
+++ day01/c/hello.c
@@ -4,23 +4,23 @@ typedef void *EFI_HANDLE;
 
 struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 typedef EFI_STATUS (*EFI_TEXT_STRING)(
-  struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *This,
-  CHAR16                                   *String);
+    struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
 
 typedef struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-  void             *dummy;
-  EFI_TEXT_STRING  OutputString;
+  void *dummy;
+  EFI_TEXT_STRING OutputString;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
 typedef struct {
-  char                             dummy[52];
-  EFI_HANDLE                       ConsoleOutHandle;
-  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *ConOut;
+  char dummy[52];
+  EFI_HANDLE ConsoleOutHandle;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
 } EFI_SYSTEM_TABLE;
 
-EFI_STATUS EfiMain(EFI_HANDLE        ImageHandle,
-                   EFI_SYSTEM_TABLE  *SystemTable) {
-  SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, world!\n");
-  while (1);
+EFI_STATUS EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  SystemTable->ConOut->OutputString(SystemTable->ConOut,
+                                    L"Hello, world from C!\n");
+  while (1)
+    ;
   return 0;
 }
